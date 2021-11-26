b11affce8dcda6edc83f7310c57f304e2f04c852
MathisVkg <mvk181@gmail.com>
2021-11-26 12:04:39 +0100

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CardItemComponent } from './card-item/card-item.component';
+import { FormItemComponent } from './form-item/form-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CardItemComponent,
+    FormItemComponent
   ],
   imports: [
     BrowserModule,
